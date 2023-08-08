5041a98d469ca55cc21ea864b6024e17a3b5a83f
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2023-08-08 18:39:29 +0530

    init project

---
+++ src/test/java/com/example/pointofsale/PointOfSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pointofsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
